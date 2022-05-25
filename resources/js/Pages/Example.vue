ed1b66088d5eff31e52ae73c4cca9b5dc7c33237
Chester Agsamosam <shapetechsolutions@gmail.com>
2022-05-25 11:54:10 +0800

    Chester's Example Page

---
+++ resources/js/Pages/Example.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+      <div>
+        <h1 class="text-4xl text-blue-500">Example</h1>
+        <p>Success! This is example page</p>
+        <p class="text-lg">Booking: {{ numberOfBookings }}</p>
+        <button @click="addBooking()" class="px-4 py-2 mr-2 text-white uppercase bg-blue-500 rounded">Add</button>
+        <button @click="addBooking()" class="px-4 py-2 text-white uppercase bg-red-500 rounded">Subtract</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            numberOfBookings: 0
+        }
+    },
+    methods: {
+        addBooking() {
+            this.numberOfBookings += 1
+        },
+
+        decrementBooking() {
+            this.numberOfBookings -= 1
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ use Inertia\Inertia;
 |
 */
 
+Route::get('/example', function () {
+    return Inertia::render('Example');
+});
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
