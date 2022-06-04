d61f97c3672587129bfa71bd76308bc68acafa29
chesteragsamosam <shapetechsolutions@gmail.com>
2022-06-04 18:40:11 +0800

    Update amadeus.js

--- resources/js/apis/amadeus.js
+++ resources/js/apis/amadeus.js
@@ -4,7 +4,8 @@ import qs from 'qs';
 const client_id = 'DEoXpcuHVV2NcNo7fcGXC8AtGFqG3ejG'
 const client_secret = 'GpuMR3MlKhsytRwX'
 const base_url = 'https://test.api.amadeus.com/v1/'
-
+const accessToken = 'H7Mb6Atkw7A1qlCLFUfv4hrPsAad'
+const Authorization = `Bearer ${accessToken}`
 
 const data = qs.stringify({
     grant_type: 'client_credentials',
@@ -22,7 +23,7 @@ const amadeus = axios.create({
     baseURL: base_url,
     headers: { 
         'content-type': 'application/x-www-form-urlencoded',
-        Authorization: 'Bearer ' + '2sT7eOyjgQLt4a6HHSzUMmYFfI1r'
+        Authorization
     }
 });
 
