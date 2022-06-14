f5fd484e018f5850ac8736eec8f54cc2efe1ce9b
Chester Agsamosam <shapetechsolutions@gmail.com>
2022-06-14 20:28:47 +0800

    add sass compatibility

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,24 @@
-require('./bootstrap');
+require("./bootstrap")
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
-import { InertiaProgress } from '@inertiajs/progress';
-import { createPinia } from "pinia";
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/inertia-vue3"
+import { InertiaProgress } from "@inertiajs/progress"
+import { createPinia } from "pinia"
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Book Thrift';
+const appName =
+  window.document.getElementsByTagName("title")[0]?.innerText ||
+  "Book Thrift";
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => require(`./Pages/${name}.vue`),
-    setup({ el, app, props, plugin }) {
-        return createApp({ render: () => h(app, props) })
-            .use(plugin)
-            .use(createPinia())
-            .mixin({ methods: { route } })
-            .mount(el);
-    },
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => require(`./Pages/${name}.vue`),
+  setup({ el, app, props, plugin }) {
+    return createApp({ render: () => h(app, props) })
+      .use(plugin)
+      .use(createPinia())
+      .mixin({ methods: { route } })
+      .mount(el);
+  },
 });
 
 InertiaProgress.init({ color: '#4B5563' });
